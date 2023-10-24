8f6d3b1e6ba98a2359b498f95c761c436faf5c04
Shanaya I. Malik <simalik@ucsc.edu>
2023-10-23 19:32:03 -0700

    Update coolness.c

--- asgn2/coolness/coolness.c
+++ asgn2/coolness/coolness.c
@@ -1,24 +1,28 @@
+/* Include the header files */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+/* Print the usage message and exit the program with error code 1 if more than two arguments are passed */
 void print_usage() {
   printf("Usage: ./coolness [temperature] [wind speed]\n");
-  exit(1); /* Exit the program with error code 1 */
+  exit(1); 
 } 
 
+/* Print the usage message and exit the program with error code 1 if either of the arguments are unacceptable */
 void print_error() {
   printf("Error: Coolness. Acceptable input values are -99<=T<=50 and   0.5<=V.\n");
   exit(1);
 }
 
+/* The formula for calculating coolness is a function of wind speed and temperature */
 float calculate_coolness(float T, float V) {
   return 35.74 + 0.6215 * T - 35.75 * pow(V, 0.16) + 0.4275 * T * pow(V, 0.16);
 }
 
+/* If no command line arguments are passed, calculate coolness for a range of parameters */
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    // No arguments
     printf("Temp\tWind\tCoolness\n");
     for (float T = -10; T <= 40; T += 10) {
       for (float V = 5; V <= 15; V += 5) {
@@ -28,7 +32,7 @@ int main(int argc, char *argv[]) {
     printf("\n");
     }
   } else if (argc == 2) {
-    // One argument
+    /* If one argument is passed, assume it is a floating point number for temperature, and print a table of calculated coolness values */
     float T = atof(argv[1]);
     if (T < -99 || T > 50) {
       print_error();
@@ -39,7 +43,7 @@ int main(int argc, char *argv[]) {
       printf("%5.1f\t%5.1f\t%5.1f\n", T, V, C);
     }
   } else if (argc == 3) {
-    // Two arguments
+        /* If two arguments are passed, the first is the temperature and the second is windspeed, assume that both inputs are valid floating point   numbers, and print a single calculated coolness value for those inputs. */
     printf("Temp\tWind\tCoolness\n");
     float T = atof(argv[1]);
     float V = atof(argv[2]);
@@ -49,8 +53,8 @@ int main(int argc, char *argv[]) {
     float C = calculate_coolness(T, V);
     printf("%5.1f\t%5.1f\t%5.1f\n", T, V, C);
   } else {
-    // Invalid number of arguments
     print_usage();
   }
-  exit(0);  // Normal exit
+  /* Exit the program with a return code of 0 */
+  exit(0);
 }
