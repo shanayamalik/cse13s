0c3eeb51aac25d9d091722b9c448c13e9b5a0e7b
Shanaya I. Malik <simalik@ucsc.edu>
2023-11-04 00:05:36 -0700

    Upload growing_arrays.c

---
+++ asgn4/growing_arrays.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Record {
+    char *name;
+    int id;
+};
+typedef struct Record Record;
+
+// to keep track of number of records and also as an array of records
+struct Table {
+    int nval;   // current number of records
+    int max;    // allocated number of records
+    Record *record; // array of records, i.e., name-id pairs
+} table;
+
+// initial size is 1, grow 2x of the existing size when it is full
+enum { INIT_SIZE = 1, GROW_SIZE = 2 };
+
+// returns the index of the item added  
+int add_record(Record record) {
+	// Your code here
+}
+
+// return 0 if there is no matching record
+// return 1 if there is a matching record and deletes it and 
+// moves records
+int del_record(const char *name) {
+	// Your code here
+}  
+
+// returns id of student record of the first matching record
+int get_id(const char *name) {
+	// Your code here
+}
+
+// returns record with a valid index, 0 to max-1 
+Record get_record(int index) {
+	// Your code here
+}
+
+// Testing code. You can modify this and check your own test cases.
+int main() {
+    table.nval = 0;
+    table.max = INIT_SIZE;
+    table.record = malloc(sizeof(Record) * table.max);
+
+    // test add_record
+    Record r1 = {"Alice", 1};
+    Record r2 = {"Bob", 2};
+    Record r3 = {"Charlie", 3};
+    int i1 = add_record(r1);
+    int i2 = add_record(r2);
+    int i3 = add_record(r3);
+    printf("added records at indices %d, %d, %d\n", i1, i2, i3);
+
+    // test get_id and get_record
+    int id = get_id("Bob");
+    Record r = get_record(i1);
+    printf("record at index %d: name=%s, id=%d\n", i1, r.name, r.id);
+    printf("record with name 'Bob': id=%d\n", id);
+
+    // test del_record
+    int success = del_record("Bob");
+    printf("deleted record with name 'Bob': %d\n", success);
+    id = get_id("Bob");
+    printf("record with name 'Bob': id=%d\n", id);
+
+    free(table.record);
+    return 0;
+}
+
+/*
+Output for the above testing code:
+
+added records at indices 0, 1, 2
+record at index 0: name=Alice, id=1
+record with name 'Bob': id=2
+deleted record with name 'Bob': 1
+record with name 'Bob': id=0
+
+
+*/
