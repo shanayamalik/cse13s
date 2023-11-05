cb9616badf0f5fe47e2bbeaf1f982645733fd85f
simalik <simalik@ucsc.edu>
2023-11-04 20:31:06 -0700

    copied from github submission

--- asgn4/growing_arrays.c
+++ asgn4/growing_arrays.c
@@ -19,25 +19,52 @@ struct Table {
 enum { INIT_SIZE = 1, GROW_SIZE = 2 };
 
 // returns the index of the item added  
-int add_record(Record record) {
-	// Your code here
+int add_record(Record new_record) {
+    if (table.nval == table.max) {
+        // The array should grow to two times the existing size
+        int new_size = table.max * GROW_SIZE;
+        Record *new_records = realloc(table.record, new_size * sizeof(Record));
+        if (!new_records) return -1;
+        table.record = new_records;
+        table.max = new_size;
+    }
+    table.record[table.nval] = new_record;
+    table.nval++;
+    return table.nval - 1;
 }
 
 // return 0 if there is no matching record
 // return 1 if there is a matching record and deletes it and 
 // moves records
 int del_record(const char *name) {
-	// Your code here
+    for (int i = 0; i < table.nval; i++) {
+        if (strcmp(table.record[i].name, name) == 0) {
+            memmove(&table.record[i], &table.record[i + 1], (table.nval - i - 1) * sizeof(Record));
+            table.nval--;
+            return 1;
+        }
+    }
+    return 0; // No matching record found
 }  
 
 // returns id of student record of the first matching record
 int get_id(const char *name) {
-	// Your code here
+    for (int i = 0; i < table.nval; i++) {
+        if (strcmp(table.record[i].name, name) == 0) {
+	    return table.record[i].id;
+        }
+    }
+    return 0;
 }
 
 // returns record with a valid index, 0 to max-1 
 Record get_record(int index) {
-	// Your code here
+    if (index >= 0 && index < table.nval) {
+        return table.record[index];
+    }
+    // If the index is invalid, return a default Record
+Record empty_record = {0};
+    return empty_record;
 }
 
 // Testing code. You can modify this and check your own test cases.
--- asgn4/ll_cycle.c
+++ asgn4/ll_cycle.c
@@ -2,5 +2,22 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+	/* Initialize two pointers at the head of the list,
+	 * with one being a slow pointer and the other being a fast pointer. */
+	node *slow_ptr = head;
+	node *fast_ptr = head;
+
+	/* There is no cycle if the list is empty or has one node. */
+	while (fast_ptr != NULL && fast_ptr->next != NULL) {
+		/* Advance the slow pointer by one node and the fast pointer by two nodes. */
+		slow_ptr = slow_ptr->next; 
+		fast_ptr = fast_ptr->next->next;
+                
+		/* If the two pointers are equal, then there is a cycle */
+		if (slow_ptr == fast_ptr) {
+			return 1;
+		}
+	}
+        /* If we get to the end, then there were no cycles. */
+	return 0;
 }
