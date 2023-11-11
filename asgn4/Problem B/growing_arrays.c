f8915cb623861f78734a889ddbf34487778a14af
simalik <simalik@ucsc.edu>
2023-11-10 22:09:16 -0800

    update

--- asgn4/Problem B/growing_arrays.c
+++ asgn4/Problem B/growing_arrays.c
@@ -3,31 +3,31 @@
 #include <string.h>
 #include "growing_arrays.h"
 
-struct Table table;
+struct Table shanaya_table;
 
 // returns the index of the item added  
 int add_record(Record new_record) {
-    if (table.nval == table.max) {
+    if (shanaya_table.nval == shanaya_table.max) {
         // The array should grow to two times the existing size
-        int new_size = table.max * GROW_SIZE;
-        Record *new_records = realloc(table.record, new_size * sizeof(Record));
+        int new_size = shanaya_table.max * GROW_SIZE;
+        Record *new_records = realloc(shanaya_table.record, new_size * sizeof(Record));
         if (!new_records) return -1;
-        table.record = new_records;
-        table.max = new_size;
+        shanaya_table.record = new_records;
+        shanaya_table.max = new_size;
     }
-    table.record[table.nval] = new_record;
-    table.nval++;
-    return table.nval - 1;
+    shanaya_table.record[shanaya_table.nval] = new_record;
+    shanaya_table.nval++;
+    return shanaya_table.nval - 1;
 }
 
 // return 0 if there is no matching record
 // return 1 if there is a matching record and deletes it and 
 // moves records
 int del_record(const char *name) {
-    for (int i = 0; i < table.nval; i++) {
-        if (strcmp(table.record[i].name, name) == 0) {
-            memmove(&table.record[i], &table.record[i + 1], (table.nval - i - 1) * sizeof(Record));
-            table.nval--;
+    for (int i = 0; i < shanaya_table.nval; i++) {
+        if (strcmp(shanaya_table.record[i].name, name) == 0) {
+            memmove(&shanaya_table.record[i], &shanaya_table.record[i + 1], (shanaya_table.nval - i - 1) * sizeof(Record));
+            shanaya_table.nval--;
             return 1;
         }
     }
@@ -36,9 +36,9 @@ int del_record(const char *name) {
 
 // returns id of student record of the first matching record
 int get_id(const char *name) {
-    for (int i = 0; i < table.nval; i++) {
-        if (strcmp(table.record[i].name, name) == 0) {
-	    return table.record[i].id;
+    for (int i = 0; i < shanaya_table.nval; i++) {
+        if (strcmp(shanaya_table.record[i].name, name) == 0) {
+	    return shanaya_table.record[i].id;
         }
     }
     return 0;
@@ -46,8 +46,8 @@ int get_id(const char *name) {
 
 // returns record with a valid index, 0 to max-1 
 Record get_record(int index) {
-    if (index >= 0 && index < table.nval) {
-        return table.record[index];
+    if (index >= 0 && index < shanaya_table.nval) {
+        return shanaya_table.record[index];
     }
     // If the index is invalid, return a default Record
 Record empty_record = {0};
@@ -57,9 +57,9 @@ Record empty_record = {0};
 
 // Testing code. You can modify this and check your own test cases.
 int main() {
-    table.nval = 0;
-    table.max = INIT_SIZE;
-    table.record = malloc(sizeof(Record) * table.max);
+    shanaya_table.nval = 0;
+    shanaya_table.max = INIT_SIZE;
+    shanaya_table.record = malloc(sizeof(Record) * shanaya_table.max);
 
     // test add_record
     Record r1 = {"Alice", 1};
@@ -82,7 +82,7 @@ int main() {
     id = get_id("Bob");
     printf("record with name 'Bob': id=%d\n", id);
 
-    free(table.record);
+    free(shanaya_table.record);
     return 0;
 }
 
