ba33235771231e4500d4c3c020f76f2428300ebd
simalik <simalik@ucsc.edu>
2023-11-17 10:02:48 -0800

    implemented an LRU ADT

--- asgn5/lru.c
+++ asgn5/lru.c
@@ -1,1 +1,95 @@
+#include "lru.h"
+#include "set.h"
+#include "hashtable.h"
 
+// Define a structure for the LRU cache
+typedef struct lru_cache {
+    // Define the maximum capacity of the cache
+    int capacity;
+    // Keep track of the current size
+    int size;
+    // A hash table for fast access
+    hashtable_t *hash_table;
+    // A set or linked list for maintaining order
+    set_t *order_set;
+} lru_cache_t;
+
+// Helper functions (definitions to be provided later)
+static void move_to_front(lru_cache_t *cache, char *key);
+static void evict_least_recently_used(lru_cache_t *cache);
+
+// Create a new LRU cache
+lru_cache_t *LRU_new(int capacity) {
+    lru_cache_t *cache = malloc(sizeof(lru_cache_t));
+    if (!cache) {
+        // Handle memory allocation failure
+        return NULL;
+    }
+
+    cache->capacity = capacity;
+    cache->size = 0;
+    cache->hash_table = hashtable_new(capacity); // Adjust based on your hashtable implementation
+    cache->order_set = set_new(); // Adjust based on your set implementation
+
+    return cache;
+}
+
+// Insert an item into the LRU cache
+void LRU_insert(lru_cache_t *cache, char *key, void *item) {
+    if (cache->size >= cache->capacity) {
+        evict_least_recently_used(cache);
+    }
+
+    // Insert the item into the hash table
+    hashtable_insert(cache->hash_table, key, item);
+
+    // Add the item to the front of the order set/linked list
+    set_insert_front(cache->order_set, key, item);
+
+    cache->size++;
+}
+
+// Find an item in the LRU cache
+void *LRU_find(lru_cache_t *cache, char *key) {
+    void *item = hashtable_find(cache->hash_table, key);
+    if (item) {
+        // Move the item to the front of the order set/linked list
+        move_to_front(cache, key);
+    }
+    return item;
+}
+
+// Delete an item from the LRU cache
+void LRU_delete(lru_cache_t *cache, char *key) {
+    hashtable_delete(cache->hash_table, key);
+    set_delete(cache->order_set, key);
+    cache->size--;
+}
+
+// Print the contents of the LRU cache
+void LRU_print(lru_cache_t *cache) {
+    // Implement based on your set and hashtable ADTs
+}
+
+// Iterate over the LRU cache
+void LRU_iterate(lru_cache_t *cache, void (*itemfunc)(void *arg, const char *key, void *item), void *arg) {
+    // Implement based on your set and hashtable ADTs
+}
+
+// Helper function to move an item to the front of the order set/linked list
+static void move_to_front(lru_cache_t *cache, char *key) {
+    // Implement based on your set ADT
+}
+
+// Helper function to evict the least recently used item
+static void evict_least_recently_used(lru_cache_t *cache) {
+    // Implement based on your set ADT
+}
+
+// Delete the LRU cache
+void LRU_delete(lru_cache_t *cache) {
+    // Clean up resources
+    hashtable_delete(cache->hash_table);
+    set_delete(cache->order_set);
+    free(cache);
+}
