14f472908e681302dd85994409c296098bda9dc6
Shanaya I. Malik <simalik@ucsc.edu>
2023-11-28 15:42:49 -0800

    Update crawler.h

--- asgn6/crawler.h
+++ asgn6/crawler.h
@@ -1,21 +1,27 @@
-#ifndef CRAWLER_H
+#ifndef CRAWLER_H          // Include guard to prevent multiple inclusions
 #define CRAWLER_H
 
-#include <stddef.h>
+#include <stddef.h>        // Include for general purpose standard library definitions
+#include <stdbool.h>       // Include for boolean data type
+#include <stdlib.h>        // Include for standard library functions
 
+// Structure definition for a webpage
 typedef struct {
-	char *url;
-	char *html;
-	size_t length;
-	int depth;
+    char *url;            // Pointer to a string containing the URL of the webpage
+    char *html;           // Pointer to a string containing the HTML content of the webpage
+    size_t length;        // Length of the HTML content
+    int depth;            // Depth of the webpage in the crawling process
 } webpage_t;
 
-typedef struct {
-	// Fill in with your implementation
-} bag_t;
+// Function prototype for malloc_with_check
+void *malloc_with_check(size_t size);   // Custom malloc function with error checking
+// Function prototype for realloc_with_check
+void *realloc_with_check(void *ptr, size_t size); // Custom realloc function with error checking
+// Function prototype for calloc_with_check
+void *calloc_with_check(size_t nmemb, size_t size); // Custom calloc function with error checking
+// Function prototype for strdup_with_check
+char *strdup_with_check(const char *s); // Custom strdup function with error checking
+// Function prototype for strndup_with_check
+char *strndup_with_check(const char *s, size_t n); // Custom strndup function with error checking
 
-typedef struct {
-	// Fill in with your implementation
-} hashtable_t;
-
-#endif
+#endif // CRAWLER_H
